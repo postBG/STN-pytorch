25031140d645c05ea008642efb5ae84b5257b31c
postBG <profile2697@gmail.com>
2018-06-08 00:28:22 +0900

    implemented localization net

---
+++ datasets.py
@@ -0,0 +1,16 @@
+import torch
+from torchvision import datasets, transforms
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST(root='.', train=True, download=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])), batch_size=64, shuffle=True, num_workers=4)
+
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST(root='.', train=False, transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])), batch_size=64, shuffle=True, num_workers=4)
---
+++ models.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class LocalizationNet(nn.Module):
+    def __init__(self):
+        super(LocalizationNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 8, 7)
+        self.conv2 = nn.Conv2d(8, 10, 5)
+        self.fc1 = nn.Linear(10 * 3 * 3, 32)
+        self.fc2 = nn.Linear(32, 3 * 2)
+
+        self.to_identity_transformation(self.fc2)
+
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), kernel_size=2, stride=2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), kernel_size=2, stride=2)
+        x = F.relu(self.fc1(x.view(-1, 10 * 3 * 3)))
+        theta = self.fc2(x).view(-1, 2, 3)
+
+        return theta
+
+    @staticmethod
+    def to_identity_transformation(fc):
+        fc.weight.data.zero_()
+        fc.bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
